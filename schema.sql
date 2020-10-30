d537e89b2231ab9b6ed43cd19074c1a6031700f8
Sam Fullerton <Samdfullerton@gmail.com>
2020-10-29 20:16:18 -0500

    created schema

---
+++ schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS  employee_trackDB;
+CREATE DATABASE employee_trackDB;
+
+USE employee_trackDB;
+
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR (30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR (30) NOT NULL,
+  salary DECIMAL,
+  FOREIGN KEY (id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR (30) NOT NULL,
+  last_name VARCHAR(30)NOT NULL ,
+  FOREIGN KEY(id) REFERENCES role(id),
+  manager_id INT 
+);
