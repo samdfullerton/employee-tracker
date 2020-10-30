0deb97fb4d526f52d805ab4dca046f34a8ac72f3
Sam Fullerton <Samdfullerton@gmail.com>
2020-10-29 21:56:24 -0500

    created data.js

--- data.js
+++ data.js
@@ -1,2 +1,74 @@
 var inquirer = require("inquirer");
-var c
+inquirer
+    .prompt([
+       {
+           type: "input",
+           name: "type",
+           message: "Do you want to create a department, role or employee?"
+       },
+       
+    ]).then(answers => {
+        if (answers.type === "department") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name:  "department",
+                    message: "please enter department"
+                }
+            ]).then(answers =>{
+                console.log(answers)
+            })
+        }else if (answers.type === "role"){
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name:  "title",
+                    message: "enter role's title"
+                },
+                {
+                    type: "input",
+                    name:  "salary",
+                    message: "enter salary"
+                },
+                {
+                    type: "input",
+                    name:  "department_id",
+                    message: "enter the department id"
+                }
+            ]).then(answers =>{
+                console.log(answers)
+            })
+        }else {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name:  "firstName",
+                    message: "enter employees first name"
+                },
+                {
+                    type: "input",
+                    name:  "lastName",
+                    message: "enter employees last name"
+                },
+                {
+                    type: "input",
+                    name:  "role_id",
+                    message: "enter employee's role id"
+                },
+                {
+                    type: "input",
+                    name:  "manager_id",
+                    message: "enter manager's id"
+                }
+            ]).then(answers =>{
+                console.log(answers)
+            })
+        }
+        
+        console.log(answers)
+    })
+    .catch(error => {
+        if(error){
+            console.error(error)
+        }
+    });
